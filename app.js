11dbd4dbd5426f72d6547d46a9cd78f30c56069e
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-10-02 21:52:15 -0300

    App changes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const foodStore = []
-let currentTab = 0
+const foodStore = [];
+let currentTab = 1;
 
 let createFood = {
   name: "",
@@ -12,28 +12,54 @@ let createFood = {
     iron: 0,
     vitaminC: 0,
     vitaminD: 0,
-  }
-}
+  },
+};
 
 function addFood(food) {
   createFood = {
     ...createFood,
-    ...food
-  }
+    ...food,
+  };
 }
 
 function addToFoodStore(foodToCreate) {
-  foodStore.push(foodToCreate)
+  foodStore.push(foodToCreate);
 }
 
 function nextTab() {
-  currentTab++
+  currentTab++;
 }
 
 function prevTab() {
-  if (currentTab === 0) return
-  currentTab--
+  if (currentTab === 0) return;
+  currentTab--;
 }
+
+function renderFoodStore() {
+  $("#foodList").empty(); 
+  $.each(foodStore, function (index, food) {
+    if (food.photo === "") {
+      food.photo = "./image-placeholder.png";
+    }
+    $("#foodList").append(`
+      <li class="food-card">
+        <img class="food-img" src="${food.photo}" alt="${food.name}" />
+        <div class="food-info">
+          <span class="food-name">${food.name}</span>
+          <span class="food-calories">${food.calories} kcal</span>
+          <span class="food-type">${food.type}</span>
+        </div>
+        <div>
+          <span>Hierro: ${food.macros.iron}</span> -
+          <span>Vit. C: ${food.macros.vitaminC}</span> -
+          <span>Vit. D: ${food.macros.vitaminD}</span>
+        </div>
+      </li>
+    `);
+  });
+}
+
+
 
 $(function () {
   const $dialog = $("#favDialog");
@@ -48,11 +74,86 @@ $(function () {
     $dialog[0].close();
   });
 
-  $("#next-tab-btn").on("click", function () {
-    nextTab()
-  })
+  if (foodStore.length === 0) {
+    $("#foodList").text("No hay comida almacenada").css("color", "grey");
+  }
 
-  $("#prev-tab-btn").on("click", function () {
-    prevTab()
-  })
+  $(".check-btn").on("click", function () {
+    $(`.check-btn`).toggleClass("check-btn-border");
+  });
+
+  $(".next-tab-btn").on("click", function () {
+    $(".next-tab-btn").text("SIGUIENTE"); // cambia el texto
+    if (currentTab === 4) {
+      $(".next-tab-btn").text("TERMINAR"); // cambia el texto
+    }
+    if (currentTab === 5) {
+      addToFoodStore(createFood);
+      currentTab = 1;
+      $(`.form-5`).addClass("form-hidden");
+      $(`.form-1`).removeClass("form-hidden");
+      console.log("FOOD STORE:", foodStore);
+      renderFoodStore();
+      $dialog[0].close();
+      return;
+    }
+    nextTab();
+    console.log(currentTab);
+    $(`.form-${currentTab - 1}`).addClass("form-hidden");
+    $(`.form-${currentTab}`).removeClass("form-hidden");
+
+    const name = $("#name").val();
+    const calories = $("#calories").val();
+    const type = $("#type").val();
+    const allergens = $("#allergens").val();
+    const portion = $("#portion").val();
+    const photo = $("#photo").val();
+    const iron = $("#iron").val();
+    const vitaminC = $("#vitaminC").val();
+    const vitaminD = $("#vitaminD").val();
+
+    addFood({
+      name,
+      calories,
+      type,
+      allergens,
+      portion,
+      photo,
+      macros: {
+        iron,
+        vitaminC,
+        vitaminD,
+      },
+    });
+
+    console.log(createFood);
+  });
+
+  $(".prev-tab-btn").on("click", function () {
+    if (currentTab === 1) return;
+    prevTab();
+    $(`.form-${currentTab + 1}`).addClass("form-hidden");
+    $(`.form-${currentTab}`).removeClass("form-hidden");
+    console.log(currentTab);
+  });
+
+  $("#photo").on("change", function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const base64Image = e.target.result;
+        $("#preview").attr("src", base64Image).show();
+
+        // 👉 acá guardamos la imagen en createFood
+        createFood = {
+          ...createFood,
+          photo: base64Image,
+        };
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
 });
